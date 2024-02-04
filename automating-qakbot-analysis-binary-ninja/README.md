8b6ec718610a00a47ead83c444a130276ef6c603
JershMagersh <jmag@pwnage.io>
2024-02-04 13:07:26 -0500

    Adding changes from 02/04/2024 stream

--- automating-qakbot-analysis-binary-ninja/README.md
+++ automating-qakbot-analysis-binary-ninja/README.md
@@ -4,7 +4,7 @@
 
 ### Binary Ninja Automation Development
 
-* We used `binaryninja.highlevelil.HighLevelILFunction.visit` to recursively enumerate function instructions to find our target HLIL call parameter (encryption key) [https://api.binary.ninja/binaryninja.highlevelil-module.html#binaryninja.highlevelil.HighLevelILFunction.visit](https://api.binary.ninja/binaryninja.highlevelil-module.html#binaryninja.highlevelil.HighLevelILFunction.visit)
+* We used `binaryninja.highlevelil.HighLevelILFunction.visit` to recursively enumerate function instructions to find our target HLIL call parameter (encryption key) [https://api.binary.ninja/binaryninja.highlevelil-module.html#binaryninja.highlevelil.HighLevelILFunction.visit](https://api.binary.ninja/binaryninja.highlevelil-module.html#binaryninja.highlevelil.HighLevelILFunction.visit) - we should strip out all of our fingerprinting code and just use recursion next stream
 
 * We found that LIEF is probably better for enumerating PE resources than pefile [https://lief-project.github.io/doc/latest/tutorials/07_pe_resource.html](https://lief-project.github.io/doc/latest/tutorials/07_pe_resource.html) - will be fixing resource extraction code next stream
 
@@ -19,3 +19,12 @@ Current state of our automation code: [extract_qakbot.py](scripts/extract_qakbot
 ![Unpacking Diagram](images/unpacking-diagram.001.png)
 
 We began analyzing the dynamic function resolution logic using Binary Ninja in Qakbot, and will work on automated function identification (through hash comparisons) and markups of function tables next stream.
+
+## Stream February 3 2024
+
+* Cleaned up code to only include recursive technique that Jordan showed us last stream
+* Fixed resource extraction code and wrote XOR code for decrypting resource
+* Confirmed Binary Refinery carve-pe works for carving second and third stage DLL
+`emit binja-decrypted-shellcode.bin | carve-pe | carve-pe > third-dll`
+* CTNordgaren cool tool for analyzing memory dumps: https://github.com/vswarte/memory-mirror
+* Smart-structures in Binary Ninja: https://docs.binary.ninja/guide/type.html#smart-structures-workflow
--- automating-qakbot-analysis-binary-ninja/scripts/extract_qakbot.py
+++ automating-qakbot-analysis-binary-ninja/scripts/extract_qakbot.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 """
 Fingerprints, identifies resource decryption function
-and extracts XOR key used to decrypt Qakbot DLL.
+and unpacks Qakbot DLL.
 
 691bc000  uint64_t sub_691bc000()
 691bc013      var_28_19
@@ -11,84 +13,100 @@ and extracts XOR key used to decrypt Qakbot DLL.
 691bc1d6      var_70_470
 691bc1d6      __builtin_memset(s: &var_70_470, c: 0, n: 0x20)
 """
-from binaryninja import HighLevelILOperation
 from binaryninja import BinaryViewType
 from binaryninja import commonil
-import pefile, binascii
+from struct import unpack
+from refinery.units.formats.pe import get_pe_size
+import pefile
+from pefile import PEFormatError, PE
 from sys import argv
+import re
 
-def extract_resource(bin_path, identifier):
-    pe = pefile.PE(bin_path)
-    pe.DIRECTORY_ENTRY_RESOURCE.entries[0].directory.entries[0].struct.Name
-    res_data = None
-    for entry in pe.DIRECTORY_ENTRY_RESOURCE.entries:
-        for inner_entry in entry.directory.entries:
-            if inner_entry.struct.Name == identifier:
-                offset = inner_entry.struct.OffsetToData
-                entire_mapped_image = pe.get_memory_mapped_image()
-                print("Size of entire mapped image: {} and offset: {}".format(len(entire_mapped_image), offset))
-                #res_data = pe.get_memory_mapped_image()[offset:offset+size-offset]
-                break
-    #print(binascii.hexlify(res_data[:256]))
-    return res_data
+found_pe = False
+
+# Based on https://binref.github.io/units/pattern/carve_pe.html
+# Thanks Rattle <3
+def carve_pe(data):
+    cursor = 0
+    mv = memoryview(data)
+    carved = []
+    while True:
+        offset = data.find(B'MZ', cursor)
+        if offset < cursor: break
+        cursor = offset + 2
+        ntoffset = mv[offset + 0x3C:offset + 0x3E]
+        if len(ntoffset) < 2:
+            return None
+        ntoffset, = unpack('H', ntoffset)
+        if mv[offset + ntoffset:offset + ntoffset + 2] != B'PE':
+            print(F'invalid NT header signature for candidate at 0x{offset:08X}')
+            continue
+        try:
+            pe = PE(data=data[offset:], fast_load=True)
+            print("Found a valid PE: {}".format(bytes(data[offset:offset+256]).hex()))
+        except PEFormatError as err:
+            print(F'parsing of PE header at 0x{offset:08X} failed:', err)
+            continue
+        pesize = get_pe_size(pe, memdump=False)
+        pedata = mv[offset:offset + pesize]
+        carved.append(bytes(pedata))
+    return carved
+
+def extract_resource(fpath, min_resource_size, rsrcid):
+    rsrc_data = None
+    pe = pefile.PE(fpath)
+    pe_mapped = pe.get_memory_mapped_image()
+    for rsrc in pe.DIRECTORY_ENTRY_RESOURCE.entries:
+        for entry in rsrc.directory.entries:
+            if entry.directory.entries[0].data.struct.Size >= min_resource_size and entry.struct.Name == rsrcid:
+                rsrc_offset = entry.directory.entries[0].data.struct.OffsetToData
+                rsrc_size = entry.directory.entries[0].data.struct.Size
+                rsrc_data = pe_mapped[rsrc_offset:rsrc_offset + rsrc_size]
+    return rsrc_data
 
 def get_key(bv):
     print("Looking for decryption key...")
     key = None
-    for f in bv.functions:
-        key = get_key_from_func(f)
-        if key:
-            break
+    for inst in bv.hlil_instructions:
+        inst.visit(visitor)
     return key
 
-def get_key_from_func(f):
-    needed_len = 5
-    insts = []
-    ci = 0
-    #For some reason not all functions have instructions?
-    try:
-        f.hlil.instructions
-    except:
-        return None
-
-    #TODO Do this with the generator index
-    for e in f.hlil.instructions:
-        insts.append(e)
-        ci += 1
-        if ci == needed_len:
-            break
-	#TODO do this at the highest bv level and use our length
-	#heuristic to avoid all fingerprinting code.
-    if len(insts) >= needed_len:
-        if(check_ops(insts)):
-            for inst in insts:
-                inst.visit(visitor)
-
 def visitor(_a, inst, _c, _d) -> bool:
+    global found_pe
+    if found_pe:
+        return False
     if isinstance(inst, commonil.Localcall):
         if len(inst.params) > 1:
-            if inst.params[1].size > 25:
-                key = inst.params[1].tokens[1]
-                inst_index = 4
+            if inst.tokens[0].text == '__builtin_strncpy':
+                print("Found target strncpy: {}".format(inst))
+                key = bytes(inst.params[1].tokens[1].text + "\x00", 'ascii') 
+                rsrc_inst_index = 4
                 rsrcid = None
-                for index, inst in enumerate(inst.function.instructions):
-                    if index == inst_index:
-                        rsrcid = inst.operands[1].value.value
-                        break
-                print("Key: {} and Resource ID: {}".format(key, rsrcid))
-        return False
+                rsrc_inst = list(inst.function.instructions)[rsrc_inst_index]
+                rsrcid = rsrc_inst.operands[1].value.value
+                path = re.sub(r"\.bndb", "", inst.function.view.file.filename)
+                rsrc_data = extract_resource(path, 1024, rsrcid)
+                print("Identified key: {} // Identified Resource ID: {}".format(key, rsrcid))
+                r = xor(key, rsrc_data)
+                carved = carve_pe(r)
+                if len(carved) >= 2:
+                    qakbot_dll = carved[1]
+                elif len(carved) == 1:
+                    qakbot_dll = carved[0]
+                fw = open("qakbot.dll", "wb")
+                fw.write(qakbot_dll)
+                fw.close()
+                found_pe = True
+                return False # Stop recursion (once we find a constant, don't recurse in to any sub-instructions (which there won't actually be any...))
 
-# Check if these instructions are calls to memset and strncpy
-# might be a brittle heuristic but need more than one sample
-def check_ops(ops):
-    return ops[1].operation == HighLevelILOperation.HLIL_CALL and \
-    ops[3].operation == HighLevelILOperation.HLIL_CALL and \
-    ops[1].operands[0].tokens[0].text == '__builtin_memset' and \
-    ops[3].operands[0].tokens[0].text == '__builtin_strncpy'
+def xor(key, ct):
+    r = bytes()
+    for i, b in enumerate(ct):
+        r += (b ^ key[i % len(key)]).to_bytes(1, 'little')
+    return r
 
 if __name__ == '__main__':
     bin_path = argv[1]
     bv = BinaryViewType['PE'].open(argv[1])
     bv.update_analysis_and_wait()
     key = get_key(bv)
-    #extract_resource(bin_path, 0x3b4) TODO: Fix this
